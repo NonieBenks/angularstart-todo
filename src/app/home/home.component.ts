83fbea7fed3db9e77c926de07258f7ee90895312
Natalia Benko <characteronme@gmail.com>
2024-09-04 12:52:23 +0300

    feat: stroing state for details;

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,18 @@
-import { Component } from '@angular/core';
-import { Todo } from '../shared/interfaces/todo';
+import { Component, inject } from '@angular/core';
+import { TodoService } from '../shared/data-access/todo.service';
 import { TodoFormComponent } from './ui/todo-form.component';
+import { TodoListComponent } from './ui/todo-list.component';
 
 @Component({
   standalone: true,
   selector: 'app-home',
   template: `<h2>Todo</h2>
-    <app-todo-form (todoSubmitted)="createTodo($event)"></app-todo-form>`,
-  imports: [TodoFormComponent],
+    <app-todo-form
+      (todoSubmitted)="this.todoService.addTodo($event)"
+    ></app-todo-form>
+    <app-todo-list [todos]="todoService.todos()"></app-todo-list>`,
+  imports: [TodoFormComponent, TodoListComponent],
 })
 export default class HomeComponent {
-  createTodo(todo: Todo) {
-    console.log(todo);
-  }
+  todoService = inject(TodoService);
 }
